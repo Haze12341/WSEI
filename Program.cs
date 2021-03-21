18cbce490b143c17514875c09acb6d227bec0f90
Haze12341 <jan.tarsa93@outlook.com>
2021-03-21 22:14:28 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            while (true)
+            {
+                string napis = Console.ReadLine();
+                if ( napis==null || napis == ""  )
+                {
+                    break;
+                }
+                int liczba = int.Parse(napis);
+                suma += liczba;
+                Console.WriteLine(suma);
+            }
+        }
+    }
+}
